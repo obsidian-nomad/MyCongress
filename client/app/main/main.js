c84d18e504398853559b57554a5f92edb5190702
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-10-20 07:03:16 -0700

    Refactored to put more logic in main.js in client/app/main

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -7,5 +7,31 @@ angular.module('myCongressApp')
         url: '/',
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
+      })
+      .state('test', {
+        url: '/test',
+        templateUrl: '/app/test.html'
+      })
+      // See angular parameter documentation here: https://github.com/angular-ui/ui-router/wiki/URL-Routing
+      .state('/profiles/{repId}', {
+        url: 'profiles/:repId',
+        templateUrl: '/app/main/repProfile.html'
+      })
+      .state('browse', {
+        url: '/browse',
+        templateUrl: '/app/main/browse.html'
       });
+  })
+  .controller('dataController', function($scope, Bills, Politicians) {
+    Bills.getBills().then(function(data){
+      $scope.bills = data;
+    });
+
+    Bills.getVotes().then(function(data){
+      $scope.votes = data;
+    });
+    //example of parameters passed into the function call
+    Politicians.getReps({'first_name': 'Adam', 'last_name': 'Schiff'}).then(function(data){
+      $scope.reps = data;
+    });
   });
