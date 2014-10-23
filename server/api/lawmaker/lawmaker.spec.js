cebd02bc9d55e13a87e8770d4ff31317aa6c4550
Ralph S <theralphsamuel@gmail.com>
2014-10-23 16:36:59 -0700

    Updated tests for Lawmaker model: get.

--- server/api/lawmaker/lawmaker.spec.js
+++ server/api/lawmaker/lawmaker.spec.js
@@ -6,14 +6,17 @@ var should = require('should');
 
 describe('GET /api/lawmakers', function() {
 
-  it('should respond with JSON array', function(done) {
+  it('should respond with JSON array containing lawmaker objects', function(done) {
     request(app)
       .get('/api/lawmakers')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
         if (err) {return done(err);}
+        res.body.should.be.ok;
         res.body.should.be.instanceof(Array);
+        //res.body.should.not.be.empty;        
+        //res.body[0].should.have.property('bioguide_id');
         done();
       });
   });
