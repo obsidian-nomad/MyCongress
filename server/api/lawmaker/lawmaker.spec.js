5bdc777e142933eff499e6e66e29ae973417dae0
Ralph S <theralphsamuel@gmail.com>
2014-10-24 03:03:19 -0700

    Added to lawmaker tests and fixed development env.

--- server/api/lawmaker/lawmaker.spec.js
+++ server/api/lawmaker/lawmaker.spec.js
@@ -15,9 +15,17 @@ describe('GET /api/lawmakers', function() {
         if (err) {return done(err);}
         res.body.should.be.ok;
         res.body.should.be.instanceof(Array);
-        //res.body.should.not.be.empty;        
-        //res.body[0].should.have.property('bioguide_id');
+        res.body.should.not.be.empty;  
+        res.body[0].should.have.properties('bioguide_id','birthday', 'chamber', 'contact_form', 'crp_id',
+          'district','facebook_id','fec_ids','first_name','gender','govtrack_id','icpsr_id','in_office', 
+          'last_name','lis_id','middle_name','name_suffix','nickname','oc_email','ocd_id','office','party',
+          'phone','senate_class','state','state_name',  'state_rank','term_end','term_start','thomas_id','title',
+          'twitter_id','votesmart_id','website','youtube_id');
         done();
       });
   });
+});
+
+describe('GET /api/lawmakers/:id', function(){
+  
 });
