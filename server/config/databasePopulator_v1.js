118ca6e1090ca994611cb09e714d51160e11d408
Ralph S <theralphsamuel@gmail.com>
2014-10-23 17:34:26 -0700

    Added method stubs and dependencies in database Populator.

--- server/config/databasePopulator_v1.js
+++ server/config/databasePopulator_v1.js
@@ -1,18 +1,42 @@
-var legislator = require('/server/api/lawmaker/lawmaker.model.js');
+var Legislator = require('/server/api/lawmaker/lawmaker.model.js');
+var https = require('http');
+var _ = require('lodash');
 
 
 
 
 //function downloads and parses all legislator data from sunlight /legislators
+//returns array of pre legislator objects
+function fetchLegislators(){
+  var results = [];
 
-//function  updates each with relevent info from influence explorer
 
 
-//ftakes array of objects and saves them as legislators to db
+  return results;
+}
+
+//function updates each legislator with relevent info from influence explorer
+function addInfluenceData(array){
+  var addTransparencyDataIds = function (array){
+    
+  };
+
+  var addContributionData = function(array){
+
+  };
+
+}
+
+function addBillsSponsorship(array){
+
+}
+
+
+//takes array of legislator objects and saves them as lawmaker models to db or updates them
 function importLegislators(array){
   for (var i = 0; i < array.length; i++) {
-    legislator.update(array[i].bioguide_id, array[i], {upsert: true}, function(err, numberAffected, rawResponse){  
-      if (err) {console.log('Error importing legislators:', err, 'Legislator: ', rawResponse);}
+    Legislator.update(array[i].bioguide_id, array[i], {upsert: true}, function(err, numberAffected, rawResponse){  
+      if (err) {console.log('Error importing legislators:', err, 'on legislator: ', rawResponse);}
       console.log('Legislator imported: ', rawResponse);
     });
   };
