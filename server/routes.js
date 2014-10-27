fe223e32b530fb1cc38bd3b287fa486ebe38495c
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-10-26 21:22:59 -0400

    Refactored to serve landing page from server.

--- server/routes.js
+++ server/routes.js
@@ -24,6 +24,10 @@ module.exports = function(app) {
    .get(errors[404]);
 
   // All other routes should redirect to the index.html
+  app.route('/welcome')
+    .get(function(req,res){
+      res.sendfile(app.get('appPath') + '/landingpage/index.html');
+    });
   app.route('/*')
     .get(function(req, res) {
       res.sendfile(app.get('appPath') + '/index.html');
