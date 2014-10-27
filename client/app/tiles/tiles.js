17c589e88ac5fdbc5893a1c5d3b37ab6ec67da6a
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-10-27 14:52:50 -0700

    Tile view css almost complete.

--- client/app/tiles/tiles.js
+++ client/app/tiles/tiles.js
@@ -3,6 +3,12 @@ angular.module('myCongress.tiles', ['myCongress.services'])
 .controller('TileController', function($scope, Politicians, Profile) {
    Politicians.getReps().then(function(data){
     $scope.bios = data.data;
+    
+    // Set Parties to full name
+    var parties = {"D": "Democrat", "R": "Republican", "I": "Independent"}
+    for( var rep in $scope.bios ){
+      $scope.bios[rep].party = parties[$scope.bios[rep].party];
+    }
    });
 
 });
