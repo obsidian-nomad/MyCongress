cf6f73d445f73a98cf65a2a97b86050d2d1199c9
willchen90 <willchen90@gmail.com>
2014-10-27 16:35:53 -0700

    fixed images to work in dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -234,7 +234,7 @@ module.exports = function (grunt) {
           src: [
             '<%= yeoman.dist %>/public/{,*/}*.js',
             '<%= yeoman.dist %>/public/{,*/}*.css',
-            '<%= yeoman.dist %>/public/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+            // '<%= yeoman.dist %>/public/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
             '<%= yeoman.dist %>/public/assets/fonts/*'
           ]
         }
