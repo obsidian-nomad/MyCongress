6fd09704087f8a390facb1fd55fd94e32a501158
mikephoran <mikephoran@gmail.com>
2014-10-28 16:17:50 -0700

    API Fixes and Image Fixes and Test Fixes

--- testing/client/main.controller.spec.js
+++ testing/client/main.controller.spec.js
@@ -1,29 +1,32 @@
-'use strict';
+//THIS TEST WAS FOR YEOMAN TEMPLATE CODE
 
-describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('myCongressApp'));
-  beforeEach(module('socketMock'));
+// 'use strict';
 
-  var MainCtrl,
-      scope,
-      $httpBackend;
+// describe('Controller: MainCtrl', function () {
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/things')
-      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
+//   // load the controller's module
+//   beforeEach(module('myCongressApp'));
+//   beforeEach(module('socketMock'));
 
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
-    });
-  }));
+//   var MainCtrl,
+//       scope,
+//       $httpBackend;
 
-  it('should attach a list of things to the scope', function () {
-    $httpBackend.flush();
-    expect(scope.awesomeThings.length).toBe(4);
-  });
-});
+//   // Initialize the controller and a mock scope
+//   beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+//     $httpBackend = _$httpBackend_;
+//     $httpBackend.expectGET('/api/things')
+//       .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
+
+//     scope = $rootScope.$new();
+//     MainCtrl = $controller('MainCtrl', {
+//       $scope: scope
+//     });
+//   }));
+
+//   it('should attach a list of things to the scope', function () {
+//     $httpBackend.flush();
+//     expect(scope.awesomeThings.length).toBe(4);
+//   });
+// });
