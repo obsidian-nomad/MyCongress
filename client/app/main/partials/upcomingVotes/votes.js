8d33a35633af12601163ee401d491030d1ba1942
Andrew Teich <andrewteich@me.com>
2014-10-30 14:25:31 -0700

    Added top donors by industry and sector

--- client/app/main/partials/upcomingVotes/votes.js
+++ client/app/main/partials/upcomingVotes/votes.js
@@ -10,12 +10,21 @@ angular.module('myCongressApp')
       });
   })
 
+  .filter('sectorNameConversion', function(sectorCodes) {
+    console.log('filter!')
+    return function(input) {
+      return sectorCodes[input];
+    };
+  })
+
   //uistateref to add multiple or duplicate partials to a page
   .controller('upcomingVotesController', function($scope, Bills, Politicians, Profile, Donors) {
     $scope.repVotes = {};
     $scope.sunriseIdToTransparencyId = {};
     $scope.transparencyIdToSunriseId = {};
     $scope.topDonorsByRep = {};
+    $scope.topIndustriesByRep = {};
+    $scope.topSectorsByRep = {};
 
     Bills.getUpcomingBills().then(function(data){
       var bills = data.data.results;
@@ -41,11 +50,24 @@ angular.module('myCongressApp')
           $scope.sunriseIdToTransparencyId[rep.bioguide_id] = transparencyId;
           $scope.transparencyIdToSunriseId[transparencyId] = rep.bioguide_id;
 
-          Donors.getTopIndustriesofPolitician(transparencyId).then(function(data){
-            console.log(data.data);
+          Donors.getTopContributorsofPolitician(transparencyId).then(function(data){
+            console.log('top contrib', data.data);
             $scope.topDonorsByRep[this.bioguide_id] = data.data;
             console.log($scope.topDonorsByRep);
           }.bind(this));
+
+          Donors.getTopSectorsofPolitician(transparencyId).then(function(data){
+            console.log('top sector ', data.data);
+            $scope.topSectorsByRep[this.bioguide_id] = data.data;
+            console.log($scope.topDonorsByRep);
+          }.bind(this));
+
+          Donors.getTopIndustriesofPolitician(transparencyId).then(function(data){
+            console.log('top industry ', data.data);
+            $scope.topIndustriesByRep[this.bioguide_id] = data.data;
+            console.log($scope.topDonorsByRep);
+          }.bind(this));
+
         }.bind(rep));
       }
       $scope.reps = senators.concat(congressmen);
--- client/components/services/Donors.service.js
+++ client/components/services/Donors.service.js
@@ -47,7 +47,7 @@ angular.module('myCongress.services')
     var key = request[0].id;
     return key;
   };
-  var _getTopIndustriesofPolitician = function (polId) {
+  var _getTopContributorsofPolitician = function (polId) {
     //might have to JSON parse based on old non-ng code
     return $http({
       method:'GET',
@@ -55,16 +55,49 @@ angular.module('myCongress.services')
     })
     .success(function(data/*, status, headers, config*/) {
       console.log('success');
-      console.log('GET POLITICIAN DONATION DATA:', data);
+      console.log('GET TOP CONTRIBUTOR DATA:', data);
       return data;
     })
     .error(function(/*data, status, headers, config*/) {
-      console.log('Error occured while getting POLITICIAN DONATION Data.');
+      console.log('Error occured while getting TOP CONTRIBUTOR Data.');
+    });
+  };
+
+  var _getTopSectorsofPolitician = function (polId){
+    return $http({
+      method:'GET',
+      url: api.transparency + 'api/1.0/aggregates/pol/'+polId+'/contributors/sectors.json' + api.key + '&type=politician&search=' + name
+    })
+    .success(function(data/*, status, headers, config*/) {
+      console.log('success');
+      console.log('GET TOP SECTOR DATA:', data);
+      return data;
+    })
+    .error(function(/*data, status, headers, config*/) {
+      console.log('Error occured while getting TOP SECTOR Data.');
+    });
+  };
+
+  var _getTopIndustriesofPolitician = function(polId){
+    return $http({
+      method:'GET',
+      url: api.transparency + 'api/1.0/aggregates/pol/'+polId+'/contributors/industries.json' + api.key + '&type=politician&search=' + name
+    })
+    .success(function(data/*, status, headers, config*/) {
+      console.log('success');
+      console.log('GET TOP INDUSTRY DATA:', data);
+      return data;
+    })
+    .error(function(/*data, status, headers, config*/) {
+      console.log('Error occured while getting TOP INDUSTRY Data.');
     });
   };
   return {
     getTopIndustriesofPolitician:_getTopIndustriesofPolitician,
     getPolitician:_getPolitician,
+    getTopSectorsofPolitician:_getTopSectorsofPolitician,
+    getTopContributorsofPolitician:_getTopContributorsofPolitician,
+    
     industries: function (id) { // id is the legislators crp_id from sunlight
       $http({
        method: 'GET',
@@ -86,6 +119,7 @@ angular.module('myCongress.services')
       })
      });
     },
+    
     individuals: function (id) {
     }
   }
