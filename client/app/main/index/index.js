43843fa268301a82c4c0536a53e2e2f102fbb83c
Michael Pinter <mpinter09@gmail.com>
2014-10-30 19:56:53 -0700

    ZipCode search from index.html is working correctly.

--- client/app/main/index/index.js
+++ client/app/main/index/index.js
@@ -2,5 +2,4 @@ angular.module('myCongress.home', ['myCongress.services'])
 
 .controller('HomeController', function($scope, Politicians) {
   $scope.zipSearch = Politicians.getRepsByZip;
-  $scope.yo = 5;
 });
